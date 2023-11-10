386bdb850c74164b44b7bed7a67f12b0f497e284
D0ri123 <lsh81550@gmail.com>
2023-11-10 18:35:43 +0900

    refactor(View): 이벤트 시행하는 월을 상수화 - 2차 구현
    월마다 이벤트를 할 수도 있으므로, 12라는 숫자도 상수로 변경했습니다.

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,10 +1,12 @@
 package christmas.view;
 
+import static christmas.util.Constants.EVENT_MONTH;
+
 public class OutputView {
-    private static final String previewEventBenefits = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String previewEventBenefits = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
 
     public void previewEventBenefits(int date) {
-        System.out.printf(previewEventBenefits, date);
+        System.out.printf(previewEventBenefits, EVENT_MONTH, date);
     }
 
 }

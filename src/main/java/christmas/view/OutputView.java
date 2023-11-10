106c8cd754aa5470639ce499850f95c1b5964e45
D0ri123 <lsh81550@gmail.com>
2023-11-10 19:30:57 +0900

    feat(OutputView): 주문한 음식 출력 기능 구현 - 2차 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,10 +3,20 @@ package christmas.view;
 import static christmas.util.Constants.EVENT_MONTH;
 
 public class OutputView {
-    private static final String previewEventBenefits = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String eventBenefits = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String orderMenuTitle = "\n\n<주문 메뉴>";
+    private static final String orderSummary = "%s %d개\n";
 
     public void previewEventBenefits(int date) {
-        System.out.printf(previewEventBenefits, EVENT_MONTH, date);
+        System.out.printf(eventBenefits, EVENT_MONTH, date);
+    }
+
+    public void printOrderMenuTitle() {
+        System.out.println(orderMenuTitle);
+    }
+
+    public void printOrderSummary(String menu, int quantity) {
+        System.out.printf(orderSummary, menu, quantity);
     }
 
 }

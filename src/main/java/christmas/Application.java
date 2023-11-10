c4622b053f3659812fd4bebede0637a7f57b8999
D0ri123 <lsh81550@gmail.com>
2023-11-10 15:26:58 +0900

    feat(Application): 전체적인 기능 구현 - 1차 구현
    핵심 키워드 클래스를 Menu와 Order로 정한 후, 주요 동작 기능을 구현했습니다.
    한 번에 모든 기능을 구현하기에는 핵심 클래스 이외에 추가 클래스 생성이
    필수적이라고 생각해서, 지금까지 생성된 객체를 바탕으로
    객체 설계 단계에서 진행한 다음 구현을 이어서 하도록 하겠습니다.

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,52 @@
 package christmas;
 
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        int day = Integer.parseInt(Console.readLine());
+
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String menu = Console.readLine().trim();
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", day);
+
+        System.out.println("\n\n<주문 메뉴>");
+        List<Order> orderedMenu = new ArrayList<>();
+
+        for (String food : menu.split(",")) {
+            int index = food.indexOf("-");
+            String foodName = food.substring(0, index);
+            int foodQuantity = Integer.parseInt(food.substring(index + 1));
+
+            orderedMenu.add(new Order(foodName, foodQuantity));
+
+            System.out.printf("%s %d개\n", foodName, foodQuantity);
+        }
+
+        int totalPriceBeforeDiscount = 0;
+        for(Order order : orderedMenu) {
+            totalPriceBeforeDiscount +=
+                Menu.getPriceWithFoodNameCondition(order.getMenu()) * order.getQuantity();
+        }
+
+        System.out.println("\n<할인 전 총주문 금액>");
+        System.out.println(totalPriceBeforeDiscount);
+
+        Menu freebie = Menu.decideWhetherToGiveFreebie(totalPriceBeforeDiscount);
+        System.out.println("\n<증정 메뉴>");
+        if(freebie.equals(Menu.FREE_CHAMPAGNE)) {
+            System.out.printf("%s %d개\n",freebie.getName(), 1);
+        } else {
+            System.out.println(freebie.getName());
+        }
+
+        System.out.println("\n<혜택 내역>");
+
+
     }
 }

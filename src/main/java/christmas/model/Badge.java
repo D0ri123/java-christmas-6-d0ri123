0d2b8a3a12dc113b3b8473b1bcb9ed82274fac63
D0ri123 <lsh81550@gmail.com>
2023-11-11 16:40:49 +0900

    feat(Badge): 뱃지 enum 클래스 구현 - 2차 구현

---
+++ src/main/java/christmas/model/Badge.java
@@ -0,0 +1,24 @@
+package christmas.model;
+
+public enum Badge {
+    SANTA(20_000, "산타"),
+    TREE(10_000, "트리"),
+    STAR(5_000, "별"),
+    NOTHING(0, "없음");
+
+    private final int minimumLimit;
+    private final String name;
+
+    Badge(int minimumLimit, String name) {
+        this.minimumLimit = minimumLimit;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMinimumLimit() {
+        return minimumLimit;
+    }
+}

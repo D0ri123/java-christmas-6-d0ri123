dfb6743f23fad64c34949d5f568e1e9c57b89e37
D0ri123 <lsh81550@gmail.com>
2023-11-11 10:28:33 +0900

    feat(OutputView): 증정 메뉴 출력 기능 구현 - 2차 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -16,6 +16,8 @@ public class Application {
         String inputDate = inputView.askExpectedVisitDate();
         OrderDate date = new OrderDate(Integer.parseInt(inputDate));
 
+        //TODO: OrderDate 만을 가지고 알 수 있는 할인 정보 ->
+
         //주문 메뉴를 입력받아 OrderGroup에 메뉴와 개수를 저장한다.
         OrderGroup inputOrder = new OrderGroup(inputView.askOrderDetails());
 
@@ -33,6 +35,21 @@ public class Application {
             totalAmountBeforeDiscount += Menu.getPriceWithFoodNameCondition(order.getMenu()) * order.getQuantity();
         }
         outputView.printTotalAmountFormatting(totalAmountBeforeDiscount);
+
+        //TODO: 증정 메뉴 - 할인 전 총주문 금액이 12만원 이상일 때, 샴페인 1개 증정
+        outputView.printFreebieMenuTitle();
+        outputView.printFreebieMenu(totalAmountBeforeDiscount);
+
+        //TODO: 혜택 내역 - 고객에게 적용된 이벤트 내역. 출력 순서는 자유롭게
+
+
+        //TODO: 총혜택 금액 = 할인 금액의 합계 + 증정 메뉴의 가격
+
+
+        //TODO: 할인 후 예상 결제 금액 = 할인 전 총주문 금액 - 할인 금액
+
+
+        //TODO: 12월 이벤트 배지 - 이벤트 배지가 부여되지 않는 경우, '없음'으로 보여주기
 
     }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,8 +8,9 @@ public class OutputView {
     private static final String eventBenefits = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String orderMenuTitle = "\n\n<주문 메뉴>";
     private static final String orderSummary = "%s %d개\n";
-    private static final String totalAmountBeforeDiscountTitle = "<할인 전 총주문 금액>";
+    private static final String totalAmountBeforeDiscountTitle = "\n<할인 전 총주문 금액>";
     private static final String moneyUnit = "%s원";
+    private static final String freebieMenuTitle = "\n\n<증정 메뉴>";
 
     public void previewEventBenefits(int date) {
         System.out.printf(eventBenefits, EVENT_MONTH, date);
@@ -28,8 +29,24 @@ public class OutputView {
     }
 
     public void printTotalAmountFormatting(int totalAmountBeforeDiscount) {
-        DecimalFormat money = new DecimalFormat("#,###");
-        String totalAmount = money.format(totalAmountBeforeDiscount);
+        String totalAmount = formattingMoney(totalAmountBeforeDiscount);
         System.out.printf(moneyUnit, totalAmount);
+    }
+
+    public void printFreebieMenuTitle() {
+        System.out.println(freebieMenuTitle);
+    }
+
+    public void printFreebieMenu(int totalAmountBeforeDiscount) {
+        if(totalAmountBeforeDiscount >= 120_000) {
+            System.out.println("샴페인 1개");
+            return;
+        }
+        System.out.println("없음");
+    }
+
+    private String formattingMoney(int totalAmount) {
+        DecimalFormat money = new DecimalFormat("#,###");
+        return money.format(totalAmount);
     }
 }

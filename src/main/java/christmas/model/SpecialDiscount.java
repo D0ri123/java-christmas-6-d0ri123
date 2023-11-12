69d1b36fabd7fb10881e0b24ebf24f46e98146e5
D0ri123 <lsh81550@gmail.com>
2023-11-12 20:01:31 +0900

    feat(SpecialDiscount): SpecialDiscount 클래스 생성 - 2차 구현
    특별 할인 클래스를 구현했습니다.

---
+++ src/main/java/christmas/model/SpecialDiscount.java
@@ -0,0 +1,42 @@
+package christmas.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpecialDiscount implements Discount {
+    private static final String eventName = "특별 할인";
+    private static final List<Integer> specialDays = new ArrayList<>(List.of(3,10,17,24,25,31));
+
+    private final boolean applicability;
+    private final int appliedPrice;
+
+    public SpecialDiscount(OrderDate orderDate, OrderGroup orderGroup) {
+        this.applicability = canApplyDiscount(orderDate);
+        this.appliedPrice = calculateDiscountAmount(orderDate, orderGroup);
+    }
+
+    @Override
+    public boolean canApplyDiscount(OrderDate orderDate) {
+        return specialDays.contains(orderDate.getDate());
+    }
+
+    @Override
+    public int calculateDiscountAmount(OrderDate orderDate, OrderGroup orders) {
+        return 1000;
+    }
+
+    @Override
+    public boolean getApplicability() {
+        return applicability;
+    }
+
+    @Override
+    public int getAppliedPrice() {
+        return appliedPrice;
+    }
+
+    @Override
+    public String getEventName() {
+        return eventName;
+    }
+}

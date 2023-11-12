620b2854ce7b10870dcd9f29b7541ab26d8c9f1f
D0ri123 <lsh81550@gmail.com>
2023-11-12 20:03:24 +0900

    feat(XmasDiscount): XmasDiscount 클래스 생성 - 2차 구현
    크리스마스 디데이 할인 클래스를 구현했습니다.

---
+++ src/main/java/christmas/model/XmasDiscount.java
@@ -0,0 +1,38 @@
+package christmas.model;
+
+public class XmasDiscount implements Discount {
+    private static final String eventName = "크리스마스 디데이 할인";
+    private final boolean applicability;
+    private final int appliedPrice;
+
+    public XmasDiscount(OrderDate orderDate, OrderGroup orders) {
+        this.applicability = canApplyDiscount(orderDate);
+        this.appliedPrice = calculateDiscountAmount(orderDate, orders);
+    }
+
+    @Override
+    public boolean canApplyDiscount(OrderDate orderDate) {
+        int date = orderDate.getDate();
+        return date >= 1 && date <= 25;
+    }
+
+    @Override
+    public int calculateDiscountAmount(OrderDate orderDate, OrderGroup orders) {
+        return 1000 + ((orderDate.getDate() - 1) * 100);
+    }
+
+    @Override
+    public boolean getApplicability() {
+        return applicability;
+    }
+
+    @Override
+    public int getAppliedPrice() {
+        return appliedPrice;
+    }
+
+    @Override
+    public String getEventName() {
+        return eventName;
+    }
+}

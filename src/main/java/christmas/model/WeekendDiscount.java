ab1ca0517fbcef4b036adfdb2bec097348cff7fc
D0ri123 <lsh81550@gmail.com>
2023-11-12 20:03:00 +0900

    feat(WeekendDiscount): WeekendDiscount 클래스 생성 - 2차 구현
    주말 할인 클래스를 구현했습니다.

---
+++ src/main/java/christmas/model/WeekendDiscount.java
@@ -0,0 +1,48 @@
+package christmas.model;
+
+import static christmas.model.Menu.Category.MAIN;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeekendDiscount implements Discount {
+    private static final String eventName = "주말 할인";
+    private static final List<String> weekends = new ArrayList<>(List.of("FRIDAY", "SATURDAY"));
+
+    private final boolean applicability;
+    private final int appliedPrice;
+
+    public WeekendDiscount(OrderDate orderDate, OrderGroup orders) {
+        this.applicability = canApplyDiscount(orderDate);
+        this.appliedPrice = calculateDiscountAmount(orderDate, orders);
+    }
+
+    @Override
+    public boolean canApplyDiscount(OrderDate orderDate) {
+        return weekends.contains(orderDate.getDay());
+    }
+
+    @Override
+    public int calculateDiscountAmount(OrderDate orderDate, OrderGroup orders) {
+        int discountedMenu = orders.getOrders().stream()
+            .mapToInt(order -> Menu.countMenu(MAIN, order.getMenu()) * order.getQuantity())
+            .sum();
+        return discountedMenu * 2023;
+    }
+
+    @Override
+    public boolean getApplicability() {
+        return applicability;
+    }
+
+    @Override
+    public int getAppliedPrice() {
+        return appliedPrice;
+    }
+
+    @Override
+    public String getEventName() {
+        return eventName;
+    }
+
+}

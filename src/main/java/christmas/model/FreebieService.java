f2f42527a4bb4ea72ab269d4297a3e23545eb3ed
D0ri123 <lsh81550@gmail.com>
2023-11-13 09:22:19 +0900

    refactor(FreebieService): 가격에 해당하는 증정품 제공 기능 구현 - 2차 구현
    기존에는 Freebie enum 클래스에서 해당 기능을 처리했습니다.
    객체는 객체대로 사용하자는 목표를 가지고,
    해당 기능을 FreebieService 클래스에서
    동작하도록 변경했습니다.

---
+++ src/main/java/christmas/model/FreebieService.java
@@ -0,0 +1,18 @@
+package christmas.model;
+
+import java.util.List;
+
+public class FreebieService {
+    private final List<Freebie> freebies;
+
+    public FreebieService(List<Freebie> freebies) {
+        this.freebies = freebies;
+    }
+
+    public  Freebie provideFreebieByPrice(int price) {
+        return freebies.stream()
+            .filter(freebie -> price >= freebie.getMinimumLimit())
+            .findFirst()
+            .orElse(Freebie.FREE_NONE);
+    }
+}

0af28eceb907154227f623ef264224617867f38f
D0ri123 <lsh81550@gmail.com>
2023-11-14 09:55:44 +0900

    refactor(OrderDate): OrderDate 도메인 클래스 수정 - 3차 구현
    도메인 내에서 getter만 남기도록 리팩토링했습니다.

--- src/main/java/christmas/model/domain/OrderDate.java
+++ src/main/java/christmas/model/domain/OrderDate.java
@@ -1,24 +1,12 @@
 package christmas.model.domain;
 
-import static christmas.util.Constants.*;
-
-import christmas.util.validator.DateValidator;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-
 public class OrderDate {
     private final int date;
     private final String day;
 
-    public OrderDate(String date) {
-        this.date = DateValidator.validate(date);
-        this.day = getDayOfWeekFromDate(this.date);
-    }
-
-    private String getDayOfWeekFromDate(int date) {
-        LocalDate localDate = LocalDate.of(LocalDate.now().getYear(), EVENT_MONTH, date);
-        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
-        return dayOfWeek.toString();
+    public OrderDate(int date, String day) {
+        this.date = date;
+        this.day = day;
     }
 
     public int getDate() {

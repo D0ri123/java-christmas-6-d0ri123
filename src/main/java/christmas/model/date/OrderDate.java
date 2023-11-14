bb32b4e35ae77077bad2a64e7f0ebeacd5a75ce4
D0ri123 <lsh81550@gmail.com>
2023-11-14 10:13:55 +0900

    refactor(OrderDate): 생성자 접근제어자 수정 - 3차 구현
    생성자의 접근제어자를 protected로 변경했습니다.
    이를 통해 외부에서 생성자를 사용할 수 있는 가능성을 제한뒀습니다.

--- src/main/java/christmas/model/date/OrderDate.java
+++ src/main/java/christmas/model/date/OrderDate.java
@@ -1,10 +1,10 @@
-package christmas.model.domain;
+package christmas.model.date;
 
 public class OrderDate {
     private final int date;
     private final String day;
 
-    public OrderDate(int date, String day) {
+    protected OrderDate(int date, String day) {
         this.date = date;
         this.day = day;
     }

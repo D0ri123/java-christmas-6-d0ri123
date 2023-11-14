5395edbc412f99415f266b4089c96418da6eab34
D0ri123 <lsh81550@gmail.com>
2023-11-14 11:57:14 +0900

    feat(OrderGroupValidator): 음료만 주문했는지 검증 기능 구현 - 3차 구현

--- src/main/java/christmas/util/validator/OrderGroupValidator.java
+++ src/main/java/christmas/util/validator/OrderGroupValidator.java
@@ -1,20 +1,16 @@
 package christmas.util.validator;
 
+import christmas.model.domain.Menu.Category;
 import christmas.model.order.Order;
+import christmas.model.service.MenuService;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class OrderGroupValidator {
-
-    public static void validateDuplicateMenu(List<Order> orders) {
-        Set<Order> orderSet = new HashSet<>(orders);
-        if (orders.size() != orderSet.size()) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
-    }
 
     public static void validateInputTemplate(String input) {
         Pattern template = Pattern.compile("([가-힣]+-[0-9]+,)*[가-힣]+-[0-9]+");
@@ -24,7 +20,20 @@ public class OrderGroupValidator {
         }
     }
 
-    public static void validateMaxOrderQuantity(List<Order> orders) {
+    public static void validateOrderGroup(List<Order> orders) {
+        validateDuplicateMenu(orders);
+        validateMaxOrderQuantity(orders);
+        validateOnlyBeverage(orders);
+    }
+
+    private static void validateDuplicateMenu(List<Order> orders) {
+        Set<Order> orderSet = new HashSet<>(orders);
+        if (orders.size() != orderSet.size()) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static void validateMaxOrderQuantity(List<Order> orders) {
         int totalQuantity = orders.stream()
             .mapToInt(Order::getQuantity)
             .sum();
@@ -33,9 +42,14 @@ public class OrderGroupValidator {
         }
     }
 
-    //TODO: 음료만 포함하는지 확인한다.
-    public static void validateOnlyBeverage(List<Order> orders) {
+    private static void validateOnlyBeverage(List<Order> orders) {
+        Set<Category> categories = orders.stream()
+            .map(order -> MenuService.findCategory(order.getMenu()))
+            .collect(Collectors.toSet());
 
+        if (categories.size() == 1 && categories.contains(Category.BEVERAGE)) {
+            throw new IllegalArgumentException("[ERROR] 음료만 주문 시, 주문할 수 없습니다. 다시 입력해 주세요.");
+        }
     }
 
 }

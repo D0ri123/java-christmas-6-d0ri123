0ad4346b0ff89472e5eb7615e75976e97a1a06d1
D0ri123 <lsh81550@gmail.com>
2023-11-14 17:54:12 +0900

    refactor(MemberBenefit): getter 메서드 이외의 메서드 제거 - 4차 구현

--- src/main/java/christmas/model/benefit/MemberBenefit.java
+++ src/main/java/christmas/model/benefit/MemberBenefit.java
@@ -3,21 +3,14 @@ package christmas.model.benefit;
 import christmas.model.discount.MemberDiscount;
 import christmas.model.freebie.Freebie;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MemberBenefit {
     private final List<MemberDiscount> appliedDiscountService;
     private final Freebie freebie;
 
     public MemberBenefit(List<MemberDiscount> discountServices, Freebie freebie) {
-        this.appliedDiscountService = filterOnlyAppliedDiscount(discountServices);
+        this.appliedDiscountService = discountServices;
         this.freebie = freebie;
-    }
-
-    private List<MemberDiscount> filterOnlyAppliedDiscount(List<MemberDiscount> discountServices) {
-        return discountServices.stream()
-            .filter(discount -> discount.getAppliedPrice() != 0)
-            .collect(Collectors.toList());
     }
 
     public List<MemberDiscount> getAppliedDiscount() {

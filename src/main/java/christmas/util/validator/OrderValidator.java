1fe74fed9e1bd8a95d4eb3ab7fe68738413069b7
D0ri123 <lsh81550@gmail.com>
2023-11-14 11:39:47 +0900

    feat(OrderValidator): 유효성 검사 클래스 생성 - 3차 구현
    존재하지 않는 메뉴를 주문하는지 검사하는 기능을 구현했습니다.
    주문 개수가 1개 미만, 20개 초과인지 검사하는 기능을 구현했습니다.

---
+++ src/main/java/christmas/util/validator/OrderValidator.java
@@ -0,0 +1,24 @@
+package christmas.util.validator;
+
+import christmas.model.domain.Menu;
+
+public class OrderValidator {
+
+    public static void validate(String input, int quantity) {
+        validateExistMenu(input);
+        validateMenuQuantity(quantity);
+    }
+
+    public static void validateExistMenu(String input) {
+        Menu.getAllMenus().stream()
+            .filter(menu -> menu.getName().equals(input))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."));
+    }
+
+    public static void validateMenuQuantity(int quantity) {
+        if(quantity < 1 || quantity > 20) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+}

95fc4a1aa28754197ee28984e4ad1db9a9d7b74a
D0ri123 <lsh81550@gmail.com>
2023-11-14 17:40:55 +0900

    refactor(InputView): view 로직 분리 - 4차 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,13 +6,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private final static String startPlanner = "안녕하세요! 우테코 식당 %d월 이벤트 플래너입니다.";
-    private final static String askVisitDate = "%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n";
+    private final static String askVisitDate = "%d월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private final static String askOrderDetails =
         "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
 
+    public void start() {
+        System.out.printf(startPlanner, EVENT_MONTH);
+    }
+
     public String askExpectedVisitDate() {
-        System.out.printf("%s\n%s", String.format(startPlanner, EVENT_MONTH), String.format(askVisitDate, EVENT_MONTH));
+        System.out.printf(askVisitDate, EVENT_MONTH);
         return Console.readLine().trim();
     }
 

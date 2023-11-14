a8c40393949170bda9aeaa42728e51a237998bc8
D0ri123 <lsh81550@gmail.com>
2023-11-14 11:56:26 +0900

    feat(MenuService): 음식 이름의 카테고리 찾는 기능 구현 - 3차 구현

--- src/main/java/christmas/model/service/MenuService.java
+++ src/main/java/christmas/model/service/MenuService.java
@@ -1,20 +1,25 @@
 package christmas.model.service;
 
 import christmas.model.domain.Menu;
+import christmas.model.domain.Menu.Category;
 import java.util.List;
 
 public class MenuService {
-    private final List<Menu> menus;
-
-    public MenuService(List<Menu> menus) {
-        this.menus = menus;
-    }
+    private static final List<Menu> menus = Menu.getAllMenus();
 
     public int getPriceWithFoodNameCondition(String foodName) {
         return menus.stream()
             .filter(menu -> menu.getName().equals(foodName))
             .findFirst()
             .map(Menu::getPrice)
+            .orElseThrow(() -> new IllegalArgumentException("찾는 음식이 없습니다."));
+    }
+
+    public static Category findCategory(String foodName) {
+        return menus.stream()
+            .filter(menu -> menu.getName().equals(foodName))
+            .findFirst()
+            .map(Menu::getCategory)
             .orElseThrow(() -> new IllegalArgumentException("찾는 음식이 없습니다."));
     }
 

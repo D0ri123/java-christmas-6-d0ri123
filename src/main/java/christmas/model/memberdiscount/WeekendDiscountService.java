813fe824f45b7c48758d3d37131a2efb6c22074e
D0ri123 <lsh81550@gmail.com>
2023-11-15 12:54:19 +0900

    refactor(WeekendDiscountService): 매직넘버 상수화 - 4차 구현

--- src/main/java/christmas/model/memberdiscount/WeekendDiscountService.java
+++ src/main/java/christmas/model/memberdiscount/WeekendDiscountService.java
@@ -1,16 +1,17 @@
 package christmas.model.memberdiscount;
 
 import static christmas.model.menu.Menu.Category.MAIN;
+import static christmas.util.Constants.MIN_DISCOUNT_AMOUNT;
 
 import christmas.model.orderdate.OrderDate;
 import christmas.model.menu.MenuService;
 import christmas.model.ordergroup.OrderGroup;
-import java.util.ArrayList;
 import java.util.List;
 
 public class WeekendDiscountService implements DiscountService {
-    private static final String eventName = "주말 할인";
-    private static final List<String> weekends = new ArrayList<>(List.of("FRIDAY", "SATURDAY"));
+    private static final String DISCOUNT_EVENT = "주말 할인";
+    private static final List<String> WEEKEND = List.of("FRIDAY", "SATURDAY");
+    private static final int WEEKEND_DISCOUNT_AMOUNT = 2_023;
 
     private final OrderDate orderDate;
     private final OrderGroup orders;
@@ -24,22 +25,22 @@ public class WeekendDiscountService implements DiscountService {
 
     @Override
     public boolean canApplyDiscount() {
-        return weekends.contains(orderDate.getDay());
+        return WEEKEND.contains(orderDate.getDay());
     }
 
     @Override
     public int calculateDiscountAmount() {
-        if (canApplyDiscount() && price >= 10_000) {
+        if (canApplyDiscount() && price >= MIN_DISCOUNT_AMOUNT) {
             int discountedMenu = orders.getOrders().stream()
                 .mapToInt(order -> MenuService.getMenuWithCategory(MAIN, order.getMenu()) * order.getQuantity())
                 .sum();
-            return discountedMenu * 2023;
+            return discountedMenu * WEEKEND_DISCOUNT_AMOUNT;
         }
         return 0;
     }
 
     @Override
     public MemberDiscount getMemberDiscount() {
-        return new MemberDiscount(eventName, calculateDiscountAmount());
+        return new MemberDiscount(DISCOUNT_EVENT, calculateDiscountAmount());
     }
 }

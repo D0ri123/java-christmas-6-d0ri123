1004791854dba5b406f99622d23a35cc99ee523e
D0ri123 <lsh81550@gmail.com>
2023-11-15 12:44:35 +0900

    chore(DiscountService): 메서드마다 공백라인 추가 - 4차 구현

--- src/main/java/christmas/model/memberdiscount/DiscountService.java
+++ src/main/java/christmas/model/memberdiscount/DiscountService.java
@@ -2,6 +2,8 @@ package christmas.model.memberdiscount;
 
 public interface DiscountService {
     boolean canApplyDiscount();
+
     int calculateDiscountAmount();
+
     MemberDiscount getMemberDiscount();
 }

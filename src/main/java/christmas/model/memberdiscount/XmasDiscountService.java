483022c9554d6aeb143026363a177e1ac0985ee8
D0ri123 <lsh81550@gmail.com>
2023-11-15 12:57:29 +0900

    refactor(XmasDiscountService): 매직넘버 상수화 - 4차 구현

--- src/main/java/christmas/model/memberdiscount/XmasDiscountService.java
+++ src/main/java/christmas/model/memberdiscount/XmasDiscountService.java
@@ -1,9 +1,16 @@
 package christmas.model.memberdiscount;
 
+import static christmas.util.Constants.MIN_DISCOUNT_AMOUNT;
+import static christmas.util.Constants.NOT_APPLIED_DISCOUNT_AMOUNT;
+
 import christmas.model.orderdate.OrderDate;
 
 public class XmasDiscountService implements DiscountService {
-    private static final String eventName = "크리스마스 디데이 할인";
+    private static final String DISCOUNT_EVENT = "크리스마스 디데이 할인";
+    private static final int EVENT_START = 1;
+    private static final int EVENT_END = 25;
+    private static final int BASE_AMOUNT = 1_000;
+    private static final int XMAS_DISCOUNT_AMOUNT = 100;
 
     private final OrderDate orderDate;
     private final int price;
@@ -16,19 +23,23 @@ public class XmasDiscountService implements DiscountService {
     @Override
     public boolean canApplyDiscount() {
         int date = orderDate.getDate();
-        return date >= 1 && date <= 25;
+        return date >= EVENT_START && date <= EVENT_END;
     }
 
     @Override
     public int calculateDiscountAmount() {
-        if (canApplyDiscount() && price >= 10_000) {
-            return 1000 + ((orderDate.getDate() - 1) * 100);
+        if (canApplyDiscount() && price >= MIN_DISCOUNT_AMOUNT) {
+            return BASE_AMOUNT + (calculateDay(orderDate.getDate()) * XMAS_DISCOUNT_AMOUNT);
         }
-        return 0;
+        return NOT_APPLIED_DISCOUNT_AMOUNT;
+    }
+
+    private int calculateDay(int date) {
+        return date - 1;
     }
 
     @Override
     public MemberDiscount getMemberDiscount() {
-        return new MemberDiscount(eventName, calculateDiscountAmount());
+        return new MemberDiscount(DISCOUNT_EVENT, calculateDiscountAmount());
     }
 }
